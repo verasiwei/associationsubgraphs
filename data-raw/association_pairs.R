dcb26f356e8cefbac673d7d6484e4f1f95af5b62
verasiwei <siweizhang0730@gmail.com>
2022-06-15 18:28:04 +0000

    add tutorial document

---
+++ data-raw/association_pairs.R
@@ -0,0 +1,3 @@
+## code to prepare `association_pairs` dataset goes here
+
+usethis::use_data(association_pairs, overwrite = TRUE)
---
+++ data-raw/phecode_def.R
@@ -0,0 +1,3 @@
+## code to prepare `phecode_def` dataset goes here
+
+usethis::use_data(phecode_def, overwrite = TRUE)
